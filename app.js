46a1a3a5d5c38814edc7458a1cef7ab5c55ef773
Findmeout <69743940+Findmeout@users.noreply.github.com>
2020-08-23 12:24:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ var firebaseConfig = {
   apiKey: "api-key",
   authDomain: "project-id.firebaseapp.com",
   databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
+  projectId: "pconst firebaseConfig = {
+  apiKey: "AIzaSyD6UuLRpTPHQWM0J5LJMD7NH6J6QjAagSY",
+  authDomain: "hacking-f6c6e.firebaseapp.com",
+  databaseURL: "https://hacking-f6c6e.firebaseio.com",
+  projectId: "hacking-f6c6e",
+  storageBucket: "hacking-f6c6e.appspot.com",
+  messagingSenderId: "360061272375",
+  appId: "1:360061272375:web:89b5a5c4169703d1f35ab2",
+  measurementId: "G-YKFQYP93V8"
+};roject-id",
   storageBucket: "project-id.appspot.com",
   messagingSenderId: "sender-id",
   appId: "app-id",
